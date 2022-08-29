b1ddc365e91d4c315462d1e426589ce84119bdfd
carolinechoi <fromfontana@gmail.com>
2022-08-29 15:07:06 -0400

    randomized colors and mapped letters

--- base.py
+++ base.py
@@ -1,4 +1,5 @@
 import fitz
+import random
 
 ### COLORS in rgb, 0 --> 1
 black = (0, 0, 0)
@@ -7,28 +8,32 @@ lilac = (0.9, 0.8, 0.9)
 blue = (0.8, 0.9, 1)
 mint = (0.8, 1, 0.7)
 
+### STORE COLORS IN A LIST
+colors = [black, pink, lilac, blue, mint]
+
+### CREATE EMPTY DICTIONARY & ALPHABET STRING
+values = {}
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+### ASSIGN COLORS TO LETTERS BY RANDOMIZING 
+for l in alphabet:
+    values[l] = colors[random.randint(0,4)]
 
 ### READ IN PDF
 doc = fitz.open("test_doc_in.pdf")
 
 for page in doc:
-    ### SEARCH
-    text = "a" 
-    text_instances = page.search_for(text) 
 
-    text2 = "t"
-    text2_instances = page.search_for(text2)
+    for key in values:
 
-    ### DRAW CIRCLE
-    for inst in text_instances:
-        circle = page.add_circle_annot(inst)
-        circle.set_colors(stroke = blue, fill = blue)
-        circle.update()
-    
-    for inst2 in text2_instances:
-        circle2 = page.add_circle_annot(inst2)
-        circle2.set_colors(stroke = pink, fill = pink)
-        circle2.update()
+        ### SEARCH
+        text_instances = page.search_for(key)
+        
+        ### DRAW CIRCLE
+        for inst in text_instances:
+            circle = page.add_circle_annot(inst)
+            circle.set_colors(stroke = values[key], fill = values[key])
+            circle.update()
 
 ### OUTPUT
 doc.save("test_doc_out.pdf", garbage=0, deflate=True, clean=True)
